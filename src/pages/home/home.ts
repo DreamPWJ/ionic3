640aa8aa2b07dfd314fdfe7a3b28796444751e8e
406798106@qq.com <406798106@qq.com>
2017-09-28 18:09:10 +0800

    ionic3基础框架

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { LoginPage } from '../login/login';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  loginPage: any;
+  params: Object;
   constructor(public navCtrl: NavController) {
-
+    this.loginPage = LoginPage;
+    this.params = { id: 42 };
   }
 
 }
