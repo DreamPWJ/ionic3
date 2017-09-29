6069f784809f78a7cd38762d94a423aeb10aed07
406798106@qq.com <406798106@qq.com>
2017-09-29 10:15:14 +0800

    ionic3基础框架

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
 /*app.component.ts 根组件 angular里的组件，就是视图，组件的产品就是我们所看见的界面。根组件主要定义了app整体的视觉表现，比如根页面、状态栏、启动界面等等*/
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import {TabsPage} from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      if (platform.is('ios')) {
+        statusBar.styleDefault();
+      } else if (platform.is('android')) {
+        statusBar.backgroundColorByHexString("#00A0FE");
+      }
       splashScreen.hide();
+
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 /*app.module.ts 根模块文件 ionic是基于angular的，而angular是完全模块化的。根模块文件是个大管家，你把你要用什么（模块、组件、服务、管道、指令等）都告诉它*/
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-import { TabsPage } from '../pages/tabs/tabs';
+import {AboutPage} from '../pages/about/about';
+import {ContactPage} from '../pages/contact/contact';
+import {HomePage} from '../pages/home/home';
+import {LoginPage} from '../pages/login/login';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp,
+      {
+        backButtonText: '',
+/*        backButtonIcon: "ios-arrow-back",*/
+        iconMode: 'ios',
+        modalEnter: 'modal-slide-in',
+        modalLeave: 'modal-slide-out',
+        tabsPlacement: 'bottom',
+        pageTransition: 'ios-transition'
+      },)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -41,4 +50,5 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
