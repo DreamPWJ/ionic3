ced19df20b3e4a140ba22a08c1763bb1b0fb694e
406798106@qq.com <406798106@qq.com>
2017-10-26 18:34:09 +0800

    ionic3基础框架

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 /*app.component.ts 根组件 angular里的组件，就是视图，组件的产品就是我们所看见的界面。根组件主要定义了app整体的视觉表现，比如根页面、状态栏、启动界面等等*/
-import {Component} from '@angular/core';
-import {Platform} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {IonicApp, Keyboard, Nav, Platform, ToastController} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 
@@ -11,8 +11,10 @@ import {TabsPage} from '../pages/tabs/tabs';
 })
 export class MyApp {
   rootPage: any = TabsPage;
+  backButtonPressed: boolean = false;  //用于判断返回键是否触发
+  @ViewChild('myNav') nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform,  statusBar: StatusBar,  splashScreen: SplashScreen, private ionicApp: IonicApp, private toastCtrl: ToastController, private keyboard: Keyboard) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -22,7 +24,47 @@ export class MyApp {
         statusBar.backgroundColorByHexString("#00A0FE");
       }
       splashScreen.hide();
-
+      this.registerBackButtonAction();//注册返回按键事件
     });
   }
+
+  registerBackButtonAction() {
+    this.platform.registerBackButtonAction(() => {
+      if (this.keyboard.isOpen()) {//如果键盘开启则隐藏键盘
+        this.keyboard.close();
+        return;
+      }
+      //如果想点击返回按钮隐藏toast或loading或Overlay就把下面加上
+      // this.ionicApp._toastPortal.getActive() || this.ionicApp._loadingPortal.getActive() || this.ionicApp._overlayPortal.getActive()
+      let activePortal = this.ionicApp._modalPortal.getActive();
+      if (activePortal) {
+        activePortal.dismiss().catch(() => {
+        });
+        activePortal.onDidDismiss(() => {
+        });
+        return;
+      }
+      let activeVC = this.nav.getActive();
+      let tabs = activeVC.instance.tabs;
+      let activeNav = tabs.getSelected();
+      return activeNav.canGoBack() ? activeNav.pop() : this.showExit()
+    }, 1);
+  }
+
+  //双击退出提示框
+  showExit() {
+    if (this.backButtonPressed) { //当触发标志为true时，即2秒内双击返回按键则退出APP
+      this.platform.exitApp();
+    } else {
+      this.toastCtrl.create({
+        message: '再按一次退出应用',
+        duration: 2000,
+        position: 'top'
+      }).present();
+      this.backButtonPressed = true;
+      setTimeout(() => this.backButtonPressed = false, 2000);//2秒内没有再次点击返回则将触发标志标记为false
+    }
+  }
+
+
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import {Tabs} from "ionic-angular";
 
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
+  @ViewChild('mainTabs') tabs:Tabs;
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
